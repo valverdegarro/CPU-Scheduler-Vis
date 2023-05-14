0fd74b95e19390f3730a2da5bca1040ce6e60fe7
Daniel Piedra <dapiedra123@gmail.com>
2023-05-14 07:07:28 -0600

    create a struct with all the information obtained from the gui to be used later. Missing validation p >= e

--- src/project_3/src/common.h
+++ src/project_3/src/common.h
@@ -6,11 +6,12 @@ typedef struct {
     int period;
 } task_config_t;
 
-
+// This structs contains everything that should be used for later use in the actual gen pdf function
 typedef struct gui_config {
     bool single_slide; // if true, generate everything in a single slide
     bool rm_enabled;
     bool edf_enabled;
     bool llf_enabled;
+    int num_tasks;
     task_config_t *task_config;
 } gui_config;
--- src/project_3/src/gui.c
+++ src/project_3/src/gui.c
@@ -3,10 +3,24 @@
 
 #include <gtk/gtk.h>
 
+// Tweak the following if you need changes in the numberic inputs
+#define MAX_TASKS 10
+#define MAX_PERIOD 1000
+#define MAX_EXEC_TIME 1000
 
 // Global vars
 GtkWidget *in_numberic;
 GtkApplication *app;
+
+typedef struct {
+    int num_tasks;
+    GtkWidget **exec_time; // array with pointers to the execution time widget for each task
+    GtkWidget **period; // array with pointers to the period time widget for each task
+    GtkWidget *button_rm; //single element to the rm check component
+    GtkWidget *button_edf; // single element to edf component
+    GtkWidget *button_llf; // single element to llf component
+    GtkWidget *button_ss; // single element to ss (single slide radio button) component
+} widgets_t;
 
 
 int grab_int_value (GtkSpinButton *button)
@@ -14,17 +28,46 @@ int grab_int_value (GtkSpinButton *button)
   return gtk_spin_button_get_value_as_int(button);
 }
 
-gui_config *prepare_config() {
+gui_config *prepare_config(widgets_t *w) {
     gui_config *config = (gui_config*) malloc(sizeof(gui_config));
-    config->task_config = (task_config_t*) malloc(sizeof(task_config_t) * );
+    config->num_tasks = w->num_tasks;
+    config->task_config = (task_config_t*) malloc(sizeof(task_config_t) * w->num_tasks);
+    config->rm_enabled = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(w->button_rm));
+    config->edf_enabled = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(w->button_edf));
+    config->llf_enabled = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(w->button_llf));
+    config->single_slide = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(w->button_ss));
+
+    for (int i = 0; i < w->num_tasks; i++) {
+        config->task_config[i].execution = gtk_spin_button_get_value_as_int(GTK_SPIN_BUTTON(w->exec_time[i]));
+        config->task_config[i].period = gtk_spin_button_get_value_as_int(GTK_SPIN_BUTTON(w->period[i]));
+    }
 
     return config;
 }
 
-void gen_pdf() {
+// INSERT YOUR CODE HERE: add the actual function that generates the pdf here
+void execute(gui_config *config) {
+    printf("ss enabled %d\n", config->single_slide);
+    printf("rm enabled %d\n", config->rm_enabled);
+    printf("edf enabled %d\n", config->edf_enabled);
+    printf("llf enabled %d\n\n", config->llf_enabled);
+
+    printf("Values for each task\n");
+    for (int i = 0; i < config->num_tasks; i++) {
+        printf("Task %d -period: %d  -exec_time: %d \n", i, config->task_config[i].period, config->task_config[i].execution);
+    }
+
+    printf("-------------------------------------\n");
+}
+
+void gen_pdf(GtkWidget *widget, gpointer user_data) {
+    widgets_t *w = (widgets_t*)user_data;
+
     // prepare config
+    gui_config *config = prepare_config(w);
 
-
+    // replace the code in the following function for the actual code
+    execute(config);
 }
 
 
@@ -33,16 +76,16 @@ void open_second_window(GtkWidget *widget, gpointer   user_data) {
     GtkWidget *window;
     GtkWidget *grid;
     GtkWidget *label;
-    GtkWidget *num_input;
     gchar *text;
-    GtkWidget *button_rm;
-    GtkWidget *button_edf;
-    GtkWidget *button_llf;
-    GtkWidget *button_ss;
     GtkWidget *button_ms ;
     GtkWidget *button_gen_pdf;
     GtkAdjustment *adjustment;
     int number_of_tasks = grab_int_value(GTK_SPIN_BUTTON(in_numberic));
+
+    widgets_t *w = (widgets_t*) malloc(sizeof(widgets_t));
+    w->exec_time = (GtkWidget **) malloc(sizeof(GtkWidget*) * number_of_tasks);
+    w->period = (GtkWidget **) malloc(sizeof(GtkWidget*) * number_of_tasks);
+    w->num_tasks = number_of_tasks;
 
     // Build the new window
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
@@ -80,15 +123,15 @@ void open_second_window(GtkWidget *widget, gpointer   user_data) {
         label = gtk_label_new(text);
         gtk_grid_attach(GTK_GRID(grid), label, 0, task_id + 1, 10, 1);
 
-        // Execution time
-        adjustment = gtk_adjustment_new(1.0, 1.0, 10.0, 1.0, 5.0, 0.0);
-        num_input = gtk_spin_button_new(adjustment, 1.0, 0);
-        gtk_grid_attach(GTK_GRID (grid), num_input, 10, task_id + 1, 10, 1);
+        // Period time
+        adjustment = gtk_adjustment_new(1.0, 1.0, MAX_PERIOD, 1.0, 5.0, 0.0);
+        w->period[task_id] = gtk_spin_button_new(adjustment, 1.0, 0);
+        gtk_grid_attach(GTK_GRID (grid), w->period[task_id], 10, task_id + 1, 10, 1);
 
-        // Period
-        adjustment = gtk_adjustment_new(1.0, 1.0, 10.0, 1.0, 5.0, 0.0);
-        num_input = gtk_spin_button_new(adjustment, 1.0, 0);
-        gtk_grid_attach(GTK_GRID(grid), num_input, 20, task_id + 1, 10, 1);
+        // Execution time
+        adjustment = gtk_adjustment_new(1.0, 1.0, MAX_EXEC_TIME, 1.0, 5.0, 0.0);
+        w->exec_time[task_id] = gtk_spin_button_new(adjustment, 1.0, 0);
+        gtk_grid_attach(GTK_GRID(grid), w->exec_time[task_id], 20, task_id + 1, 10, 1);
     }
 
     /*
@@ -96,29 +139,25 @@ void open_second_window(GtkWidget *widget, gpointer   user_data) {
     */
 
     // Algorithms buttons
-    button_rm = gtk_check_button_new_with_label ("RM");
-    //g_signal_connect (button_rm, "toggled", G_CALLBACK (cb_toggle_bit), rm);
-    gtk_grid_attach(GTK_GRID (grid), button_rm , 0, number_of_tasks+1, 10, 1);
+    w->button_rm = gtk_check_button_new_with_label ("RM");
+    gtk_grid_attach(GTK_GRID (grid), w->button_rm , 0, number_of_tasks+1, 10, 1);
 
-    button_edf = gtk_check_button_new_with_label ("EDF");
-    //g_signal_connect (button_edf, "toggled", G_CALLBACK (cb_toggle_bit), edf);
-    gtk_grid_attach(GTK_GRID(grid), button_edf, 10, number_of_tasks+1, 10, 1);
+    w->button_edf = gtk_check_button_new_with_label ("EDF");
+    gtk_grid_attach(GTK_GRID(grid), w->button_edf, 10, number_of_tasks+1, 10, 1);
 
-    button_llf = gtk_check_button_new_with_label ("LLF");
-    //g_signal_connect (button_llf, "toggled", G_CALLBACK (cb_toggle_bit), llf);
-    gtk_grid_attach(GTK_GRID (grid), button_llf, 20, number_of_tasks+1, 10, 1);
+    w->button_llf = gtk_check_button_new_with_label ("LLF");
+    gtk_grid_attach(GTK_GRID (grid), w->button_llf, 20, number_of_tasks+1, 10, 1);
 
 
     // radio buttons for slides
     button_ms = gtk_radio_button_new_with_label_from_widget (NULL, "Multiples Slides");
     gtk_grid_attach (GTK_GRID (grid), button_ms, 0, number_of_tasks + 2, 10, 1);
-    button_ss = gtk_radio_button_new_with_label_from_widget (GTK_RADIO_BUTTON(button_ms), "Unico Slide");
-    gtk_grid_attach (GTK_GRID (grid), button_ss, 15, number_of_tasks + 2, 15, 1);
-    //g_signal_connect (button_ms, "clicked", G_CALLBACK (cb_toggle_bit), multiple_slides);
+    w->button_ss = gtk_radio_button_new_with_label_from_widget (GTK_RADIO_BUTTON(button_ms), "Unico Slide");
+    gtk_grid_attach (GTK_GRID (grid), w->button_ss, 15, number_of_tasks + 2, 15, 1);
 
     // gen pdf button
     button_gen_pdf = gtk_button_new_with_label ("Generar .pdf");
-    //g_signal_connect (button_gen_pdf, "clicked", G_CALLBACK (cb_generate_pdf), config);
+    g_signal_connect(button_gen_pdf, "clicked", G_CALLBACK (gen_pdf), w);
     gtk_grid_attach (GTK_GRID (grid), button_gen_pdf, 0, number_of_tasks + 3, 15, 1);
 
     // Show new window
@@ -155,7 +194,7 @@ void open_first_window() {
     gtk_grid_attach(GTK_GRID(grid), label, 0, 9, 6, 1);
 
     // An input the number of tasks to create as a sping button
-    adjustment = gtk_adjustment_new(1.0, 1.0, 10.0, 1.0, 5.0, 0.0);
+    adjustment = gtk_adjustment_new(1.0, 1.0, MAX_TASKS, 1.0, 5.0, 0.0);
     in_numberic = gtk_spin_button_new(adjustment, 1.0, 0);
     gtk_grid_attach(GTK_GRID (grid), in_numberic, 2, 11, 2, 1);
 

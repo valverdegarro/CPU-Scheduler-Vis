5994c68835337584578d862e148c4f7c455e6bb5
David V <dval7788@gmail.com>
2023-05-28 12:26:48 -0600

    Modifying ttables to divide charts with a big lcm into multiple slides.

--- src/project_3/src/main.c
+++ src/project_3/src/main.c
@@ -4,8 +4,6 @@
 
 #include "gui.h"
 
-#define MCM 26
-#define NTASKS 3
 
 // Set the following define to 1 if you want to compile with the GUI, 0 otherwise
 // non GUI mode is used for development (faster than using the GUI to execute desired code)
@@ -48,22 +46,30 @@ int main(int argc, char **argv)
     // tasks[2].period = 8;
 
     // EDF data from https://microcontrollerslab.com/earliest-deadline-first-scheduling/
-    tasks[0].execution = 1;
-    tasks[0].period = 4;
-    tasks[1].execution = 2;
-    tasks[1].period = 6;
-    tasks[2].execution = 3;
-    tasks[2].period = 8;
+    //tasks[0].execution = 1;
+    //tasks[0].period = 4;
+    //tasks[1].execution = 2;
+    //tasks[1].period = 6;
+    //tasks[2].execution = 3;
+    //tasks[2].period = 8;
 
     // LLF data from https://microcontrollerslab.com/least-laxity-first-llf/
-    // NOTE: the graph for this is barely readable, i'll work on a fix in ttables.c
+    // NOTE: the lcm for this set is 120 so it will generate a lot of slides
+    tasks[0].execution = 2;
+    tasks[0].period = 6;
+    tasks[1].execution = 2;
+    tasks[1].period = 8;
+    tasks[2].execution = 3;
+    tasks[2].period = 10;
 
-    //tasks[0].execution = 2;
-    //tasks[0].period = 6;
+
+    // Test data for more than 100% CPU utilization
+    //tasks[0].execution = 1;
+    //tasks[0].period = 1;
     //tasks[1].execution = 2;
-    //tasks[1].period = 8;
+    //tasks[1].period = 50;
     //tasks[2].execution = 3;
-    //tasks[2].period = 10;
+    //tasks[2].period = 50;
 
     gui_config config;
     config.single_slide = true;
@@ -79,53 +85,5 @@ int main(int argc, char **argv)
     #else
     return init_gui(argc, argv);
     #endif
-
-    
-    /*char buffer[4096];
-    memset(buffer, 0, 4096);
-    
-    // Ugly code to initialize timeslots array manually
-    int tasks[MCM] = {0,1,1,2,2,2,0,2,2,1,1,2,0,-1,-1,-1,-1,-1,0,1,1,2,2,2,0,2};
-    //int tasks[MCM] = {0,1,2,3,4,5,6,7,8,9,1,2,0,4,9,6,3,-1,0,-1,-1,2,5,2,7,2}; // 10 tasks example
-
-    timeslot_t *ts_array = malloc(MCM * sizeof(timeslot_t));
-
-    for (int i = 0; i < MCM; i++) {
-        ts_array[i].task_id = tasks[i];
-
-        ts_array[i].deadlines = malloc(NTASKS * sizeof(bool));
-        //memcpy(ts_array[i].deadlines, deadlines, NTASKS);
-
-        for (int j = 0; j < NTASKS; j++) {
-            ts_array[i].deadlines[j] = false;
-        }
-    }
-
-    // Manually set some deadlines
-    // Task 1
-    ts_array[0].deadlines[0] = true;
-    ts_array[6].deadlines[0] = true;
-    ts_array[12].deadlines[0] = true;
-    ts_array[18].deadlines[0] = true;
-    ts_array[24].deadlines[0] = true;
-
-    // Task 2
-    ts_array[0].deadlines[1] = true;
-    ts_array[9].deadlines[1] = true;
-    ts_array[18].deadlines[1] = true;
-
-    // Task 3
-    ts_array[0].deadlines[2] = true;
-    ts_array[18].deadlines[2] = true;
-    
-
-    // Write some missed deadlines
-    bool misses[] = {false, true, true};
-    int miss_idx = 18;
-
-    write_ttable(buffer, ts_array, MCM, NTASKS, misses, miss_idx);
-    printf("%s\n", buffer);
-
-    //return init_gui(argc, argv);*/
 
 }
